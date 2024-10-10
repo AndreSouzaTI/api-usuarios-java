bfa89fec0666168c61e9458c169563810a1cbed0
André Souza <andresouza_ti@yahoo.com>
2024-10-10 16:00:33 -0300

    Iniciando projeto Java/Spring Boot

---
+++ src/main/java/com/example/APIgerenciamentodeusuarios/ApIgerenciamentodeusuariosApplication.java
@@ -0,0 +1,13 @@
+package com.example.APIgerenciamentodeusuarios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApIgerenciamentodeusuariosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApIgerenciamentodeusuariosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/APIgerenciamentodeusuarios/ApIgerenciamentodeusuariosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.APIgerenciamentodeusuarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApIgerenciamentodeusuariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
